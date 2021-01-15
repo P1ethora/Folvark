fba56c548ab164e12362b944b1a6432905a29cfc
ihairrr@gmail.com <ihairrr@gmail.com>
2021-01-15 03:53:08 +0300

    added main page

---
+++ src/main/java/net/plethora/folvark/controller/MainPageController.java
@@ -0,0 +1,14 @@
+package net.plethora.folvark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainPageController {
+
+    @GetMapping("/main")
+    public String getMain() {
+
+        return "main-page";
+    }
+}
