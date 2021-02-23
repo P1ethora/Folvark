2237326929c819fc27df4765e9aba013cc4f8eda
ihairrr@gmail.com <ihairrr@gmail.com>
2021-02-24 00:09:42 +0300

    added "send email"

--- src/main/java/net/plethora/folvark/controller/MainPageController.java
+++ src/main/java/net/plethora/folvark/controller/MainPageController.java
@@ -13,6 +13,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.servlet.http.HttpSession;
 import java.util.List;
@@ -47,8 +49,9 @@ public class MainPageController {
     }
 
     @PostMapping("/addEmail")
-    public void addEmail(String email) {
-        System.out.println(email);
+    public @ResponseBody
+    void addEmail(@RequestBody String eml) {
+        String email = eml.replace("\"", "");
         daoEmailUser.saveEmail(new EmailUser(email));
     }
 
