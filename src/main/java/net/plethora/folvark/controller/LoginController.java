227c84cdf1a644e735bffd633e59167294381d14
ihairrr@gmail.com <ihairrr@gmail.com>
2021-03-03 00:09:10 +0300

    added register-page

---
+++ src/main/java/net/plethora/folvark/controller/LoginController.java
@@ -0,0 +1,15 @@
+package net.plethora.folvark.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/register")
+    public String openRegister() {
+
+        return "registration-page";
+    }
+
+}
