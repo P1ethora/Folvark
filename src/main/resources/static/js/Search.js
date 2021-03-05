f4c1ed52c1cc118e24043656910f2fe59f96bfe4
ihairrr@gmail.com <ihairrr@gmail.com>
2021-03-05 22:56:27 +0300

    upgrade search panel js

--- src/main/resources/static/js/Search.js
+++ src/main/resources/static/js/Search.js
@@ -13,15 +13,28 @@ function ser(element) {
     console.log("text");
     const input = element.value;
     document.getElementById('sugg').innerHTML = '';
+    document.getElementById('field_sear').style.borderBottomLeftRadius = "0";
+    document.getElementById('field_sear').style.borderTopLeftRadius = "0";
     const suggestions = countries.filter(function (country) {
         return country.name.toLowerCase().startsWith(input);
     });
+
+    if (suggestions.length === 0) {
+        const div = document.createElement('div');
+        div.className = 'nothing-search';
+        div.innerHTML = 'Не найдено';
+
+        document.getElementById('sugg').appendChild(div);
+    }
+
     suggestions.forEach(function (suggested) {
         const div = document.createElement('div');
         div.innerHTML = suggested.name;
         document.getElementById('sugg').appendChild(div);
     });
     if (input === '') {
+        document.getElementById('field_sear').style.borderBottomLeftRadius = "30px";
+        document.getElementById('field_sear').style.borderTopLeftRadius = "30px";
         document.getElementById('sugg').innerHTML = '';
     }
 }
