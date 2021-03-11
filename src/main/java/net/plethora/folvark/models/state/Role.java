b93235dd770575f3cae8f2de602f4e11bd6c2004
ihairrr@gmail.com <ihairrr@gmail.com>
2021-03-12 02:06:40 +0300

    translated to jdk9, added role

--- src/main/java/net/plethora/folvark/models/state/Role.java
+++ src/main/java/net/plethora/folvark/models/state/Role.java
@@ -1,6 +1,29 @@
 package net.plethora.folvark.models.state;
 
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
 public enum Role {
-    USER,
-    ADMIN
+    USER(Set.of(Permission.DEVELOPERS_READ)),
+    ADMIN(Set.of(Permission.DEVELOPERS_READ));
+
+    private final Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermission() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermission().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
+
 }
---
+++ src/main/java/net/plethora/folvark/security/SecurityUser.java
@@ -0,0 +1,70 @@
+package net.plethora.folvark.security;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import net.plethora.folvark.models.User;
+import net.plethora.folvark.models.state.Status;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class SecurityUser implements UserDetails {
+
+    private String username;
+    private String password;
+    private List<SimpleGrantedAuthority> authorities;
+    private boolean isActive;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return isActive;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isActive;
+    }
+
+    public static UserDetails fromUser(User user) {
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getStatus().equals(Status.ACTIVE),
+                user.getRole().getAuthorities()
+        );
+    }
+
+}
