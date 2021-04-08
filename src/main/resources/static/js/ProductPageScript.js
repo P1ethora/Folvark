812273289a1331ef130c0f9b7c6aa70d42f7fb95
ihairrr@gmail.com <ihairrr@gmail.com>
2021-04-09 01:54:31 +0300

    update reply comment (2)

--- src/main/resources/static/js/ProductPageScript.js
+++ src/main/resources/static/js/ProductPageScript.js
@@ -80,17 +80,16 @@ $(function () {
 
 function answerToComment(button) {
 
-    let blockWithComments = document.getElementsByClassName('block_with_comments').item(0);
-    let that = $(button).closest('.product-comment');
+    let that = button.closest('.product-comment');
 
-    if (blockWithComments.getElementsByClassName('send-comment').item(0) != null) {
-        blockWithComments.getElementsByClassName('send-comment').item(0).remove();
+    if (document.getElementsByClassName('send-comment').item(0) != null) {
+        document.getElementsByClassName('send-comment').item(0).remove();
     }
 
     let sendComments = document.createElement('div');
     sendComments.className = 'send-comment';
     sendComments.id = 'wrb1';
-    sendComments.setAttribute('parent', that.attr('id'));
+    sendComments.setAttribute('parent', that.getAttribute('id'));
 
     let topSendComment = document.createElement('div');
     topSendComment.className = 'top-send-comment';
@@ -125,7 +124,7 @@ function answerToComment(button) {
     sendComments.appendChild(topSendComment);
     sendComments.appendChild(bottomSendComment);
 
-    blockWithComments.insertBefore(sendComments, document.getElementById($(that).attr("id")).nextSibling);
+    that.insertBefore(sendComments, that.getElementsByClassName('open_reply').item(0));
 
     // let valueComment = commentNameUser.value;
 
@@ -142,11 +141,12 @@ function answerToComment(button) {
 
 function sendReplyComment(button) {
     // let blockWithComments = document.getElementsByClassName('block_with_comments').item(0);
-    let that = $(button).closest('.send-comment');
+    let that = button.closest('.send-comment');
+    let comment = that.closest('.product-comment');
     let url = document.location.href;
-    let commentFromReply = document.getElementById($(that).attr('parent'));
+    // let commentFromReply = document.getElementById($(that).attr('parent'));
 
-    let list = commentFromReply.getElementsByClassName('reply_block').item(0);
+    let replyList = comment.getElementsByClassName('reply_block').item(0);
 
     // let response = fetch(url + '/addComment', {
     //     method: 'POST',
@@ -171,17 +171,18 @@ function sendReplyComment(button) {
     let buttonReply = document.createElement('button');
 
 
-    productComment.className = 'product-comment';
-    imgComment.className = 'photo-user';
-    commentBlock.className = 'comment-block';
-    topComment.className = 'top-comment';
-    commentName.className = 'comment-name';
-    commentData.className = 'comment-data';
-    commentText.className = 'comment-text';
-    commentAnswer.className = 'comment-answer';
-    buttonReply.className = 'reply-to-comment';
+    productComment.className = 'reply-to-comment';
+    imgComment.className = 'reply-photo-user';
+    commentBlock.className = 'reply-comment-block';
+    topComment.className = 'reply-top-comment';
+    commentName.className = 'reply-comment-name';
+    commentData.className = 'reply-comment-data';
+    commentText.className = 'reply-comment-text';
+    commentAnswer.className = 'reply-comment-answer';
+    buttonReply.className = 'btn-reply-comment';
 
     buttonReply.innerHTML = 'Ответить';
+    buttonReply.setAttribute('onclick', "replyToReply(this)");
     // commentText.innerHTML = commentFromReply.getElementsByClassName("text-comment").item(0).value;
     commentText.innerHTML = document.getElementById('txt-comment-reply').value;
 
@@ -198,14 +199,70 @@ function sendReplyComment(button) {
     productComment.appendChild(imgComment);
     productComment.appendChild(commentBlock);
 
-    list.appendChild(productComment)
+    replyList.appendChild(productComment);
+    that.remove();
+    replyList.style.display = 'flex';
 
 }
 
 function openListReply(button) {
-    let that = $(button).closest('.product-comment');
-    let list = document.getElementById($(that).attr('id')).getElementsByClassName('reply_block').item(0);
-
+    let that = button.closest('.product-comment');
+    let list = that.getElementsByClassName('reply_block').item(0);
     list.style.display = 'flex';
 }
 
+function closeListReply(button) {
+    let that = button.closest('.product-comment');
+    let list = that.getElementsByClassName('reply_block').item(0);
+    list.style.display = 'none';
+}
+
+function replyToReply(button) {
+    if (document.getElementsByClassName('send-comment').item(0) != null) {
+        document.getElementsByClassName('send-comment').item(0).remove();
+    }
+    let that = button.closest('.reply-to-comment');
+
+
+    let sendComments = document.createElement('div');
+    sendComments.className = 'send-comment';
+    sendComments.id = 'wrb1';
+    sendComments.setAttribute('parent', that.getAttribute('id'));
+
+    let topSendComment = document.createElement('div');
+    topSendComment.className = 'top-send-comment';
+
+    let imgProfile = document.createElement('img');
+    imgProfile.className = 'imgProfile';
+
+    let textComment = document.createElement('input');
+    textComment.className = 'text-comment';
+    textComment.id = 'txt-comment-reply';
+
+    let bottomSendComment = document.createElement('div');
+    bottomSendComment.className = 'bottom-send-comment';
+
+    let btnSendCancel = document.createElement('div');
+    btnSendCancel.className = 'btn-send-cancel';
+    btnSendCancel.innerHTML = 'Отмена';
+    btnSendCancel.setAttribute('onclick', 'cancelReplyComment()');
+
+
+    let btnSendComment = document.createElement('div');
+    btnSendComment.className = 'btn-send-comment';
+    btnSendComment.innerHTML = 'Оставить отзыв';
+    // btnSendComment.setAttribute('onclick', "sendReplyComment(this)");
+
+
+    topSendComment.appendChild(imgProfile);
+    topSendComment.appendChild(textComment);
+
+    bottomSendComment.appendChild(btnSendCancel);
+    bottomSendComment.appendChild(btnSendComment);
+
+    sendComments.appendChild(topSendComment);
+    sendComments.appendChild(bottomSendComment);
+
+    that.appendChild(sendComments);
+
+}
